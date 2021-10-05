30026ae80974249c770903200cbad97cf1a51e21
gytks4 <gytks4@naver.com>
2021-10-06 00:05:38 +0900

    feat(write): tag 추가 함수 구현

--- velog-clone/write.js
+++ velog-clone/write.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const tags = document.querySelector(".tags");
+const input = document.querySelector(".input");
+
+input.addEventListener("keydown", addTag);
+
+function addTag(e) {
+  console.log(`e.key`, e.key);
+  if (e.key === "Enter") {
+    const span = document.createElement("span");
+    span.setAttribute("class", "tag");
+    span.innerText = e.target.value;
+    e.tarvet.value = "";
+    tags.appendChild(span);
+  }
+}
