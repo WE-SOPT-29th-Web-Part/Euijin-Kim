c95d37b227f52f3d6e4efd11e652e506c9633cb2
gytks4 <gytks4@naver.com>
2021-10-05 20:27:05 +0900

    feat: card 모달 구현

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -18,3 +18,33 @@ navDropdownItems.forEach((item) =>
     navPeriodText.innerText = item.innerText;
   })
 );
+
+const cardContainer = document.querySelector(".cards");
+const cards = document.querySelectorAll(".card");
+
+cardContainer.addEventListener("click", (e) => {
+  let el = e.target;
+  // walk up the tree until we find a LI item
+  while (el && !el.classList.contains("card")) {
+    el = el.parentNode;
+  }
+  if (el.classList.contains("card__modal")) return;
+  const modalBg = document.createElement("div");
+  const modalCard = document.createElement("article");
+  modalBg.appendChild(modalCard);
+  modalBg.setAttribute("class", "card__modal-bg");
+  cardContainer.appendChild(modalBg);
+  modalCard.innerHTML = el.innerHTML;
+  modalCard.setAttribute("class", "card card__modal");
+
+  const modalCloseBtn = document.createElement("button");
+  modalCloseBtn.innerText = "X";
+  modalCloseBtn.setAttribute("class", "card__modal-close-btn");
+  modalCard.appendChild(modalCloseBtn);
+  document.body.style.overflow = "hidden";
+
+  modalCloseBtn.addEventListener("click", () => {
+    modalBg.remove();
+    document.body.style.overflow = "visible";
+  });
+});
