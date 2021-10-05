97660b5fdbcd022ff0a4f55379e16aadc5628681
gytks4 <gytks4@naver.com>
2021-10-05 17:44:06 +0900

    feat: dropdown text 변경하는 함수 추가

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -2,17 +2,19 @@
 
 const navPeriod = document.querySelector(".nav__period");
 const navDropdown = document.querySelector(".nav__dropdown");
+const navPeriodText = document.querySelector(".nav__period-text");
+const navDropdownItems = document.querySelectorAll(".nav__dropdown > div");
+navPeriod.addEventListener("click", () => handleDropdown(navDropdown));
 
-navPeriod.addEventListener("click", handleDropdown);
-
-function handleDropdown(e) {
-  //   if (navDropdown.classList.contains("visible")) {
-  //     navDropdown.classList.remove("visible");
-  //   } else {
-  //     navDropdown.classList.add("visible");
-  //   }
-
-  navDropdown.classList.contains("visible")
-    ? navDropdown.classList.remove("visible")
-    : navDropdown.classList.add("visible");
+function handleDropdown(element) {
+  element.classList.toggle("visible");
+  //   element.classList.contains("visible")
+  //     ? element.classList.remove("visible")
+  //     : element.classList.add("visible");
 }
+
+navDropdownItems.forEach((item) =>
+  item.addEventListener("click", () => {
+    navPeriodText.innerText = item.innerText;
+  })
+);
