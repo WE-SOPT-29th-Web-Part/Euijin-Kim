f680f8af548ad536e910bdd497cb243724cc6194
gytks4 <gytks4@naver.com>
2021-10-19 02:23:08 +0900

    feat: header 구현

---
+++ react-velog-clone/src/libs/constants/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  mainWhite: "rgb(248, 249, 250)",
+  mainBlack: "rgb(52, 58, 64)",
+  mainGreen: "rgb(32, 201, 151)",
+  darkBlack: "rgb(33, 37, 41)",
+  lightGray: "rgb(73, 80, 87)",
+};
--- react-velog-clone/src/pages/Main.jsx
+++ react-velog-clone/src/pages/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/common/Header";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Main;
