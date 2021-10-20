6345b6b22927990e1284e5d394ef4a5ca097b6d9
gytks4 <gytks4@naver.com>
2021-10-20 11:15:27 +0900

    feat: JSX 구조 완성

--- d-day-calc/src/App.js
+++ d-day-calc/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import Header from "./components/common/Header";
+import styled from "styled-components";
+import DateInput from "./components/DateInput";
+import Result from "./components/Result";
+import Footer from "./components/common/Footer";
 
 function App() {
-  return <div>3rd Seminar</div>;
+  return (
+    <div>
+      <div id="home__top">
+        <Header />
+        <DateInput />
+      </div>
+      <div id="home__middle-line"></div>
+      <div className="home__bottom">
+        <Result />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
