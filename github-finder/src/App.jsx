8a561a4c0c2904415e37cacfaa5c17edc6b6e71d
gytks4 <gytks4@naver.com>
2021-10-21 01:48:56 +0900

    [react-router-dom 준비]

--- github-finder/src/App.jsx
+++ github-finder/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/common/Header";
+import History from "./pages/History";
 import Home from "./pages/Home";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={() => <Home />} />
+          <Route exact path="/history" component={() => <History />} />
           <Route component={() => <div>Fage Not Found</div>} />
         </Switch>
       </BrowserRouter>
