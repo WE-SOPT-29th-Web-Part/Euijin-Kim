c6cf1fb1c1ea9d14c5e8271852c295d43e1e06e1
gytks4 <gytks4@naver.com>
2021-10-21 02:33:59 +0900

    feat: searchBar 구현, api 받아오기

--- github-finder/src/App.jsx
+++ github-finder/src/App.jsx
@@ -3,13 +3,27 @@ import Header from "./components/common/Header";
 import SearchBar from "./components/SearchBar";
 import styled from "styled-components";
 import Result from "./components/Result";
+import { useState } from "react";
+import { client } from "./libs/api";
 
 function App() {
+  const [userInfo, setUserInfo] = useState();
+
+  const getUserInfo = async (user) => {
+    try {
+      const { data } = await client.get(`/${user}`);
+      console.log(`data`, data);
+      setUserInfo(data);
+    } catch (e) {
+      console.log(`e`, e);
+    }
+  };
+
   return (
     <StyledRoot>
       <Header />
-      <SearchBar />
-      <Result />
+      <SearchBar getUserInfo={getUserInfo} />
+      <Result userInfo={userInfo} />
     </StyledRoot>
   );
 }
--- github-finder/src/components/SearchBar.jsx
+++ github-finder/src/components/SearchBar.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ getUserInfo }) => {
+  const [user, setUser] = useState();
+
+  const handleChange = (e) => {
+    setUser(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getUserInfo(user);
+    setUser("");
+  };
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledInput
+        type="text"
+        placeholder="Github 프로필을 검색해보세요."
+        onChange={handleChange}
+        value={user || ""}
+      />
+    </StyledForm>
+  );
 };
 
 export default SearchBar;
+
+const StyledForm = styled.form``;
+
+const StyledInput = styled.input`
+  border: none;
+  outline: none;
+  border-radius: 10px;
+  width: 320px;
+  height: 57px;
+  background-color: #24272b;
+  color: #e5e6e7;
+  padding: 16px;
+`;
