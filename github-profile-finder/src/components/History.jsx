3e6c1f593498f22f3d6edb0b88dc6061075032a7
gytks4 <gytks4@naver.com>
2021-10-21 16:47:25 +0900

    feat: history 삭제 함수 구현

--- github-profile-finder/src/components/History.jsx
+++ github-profile-finder/src/components/History.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
-const History = ({ userList }) => {
-  return (
-    <div>
-      {userList.map((user) => (
-        <>
-          <div>{user}</div>
-          <button>X</button>
-        </>
-      ))}
+const History = ({ userList, handleRemove }) => {
+  return userList.map((user) => (
+    <div key={user}>
+      <div>{user}</div>
+      <button onClick={() => handleRemove(user)}>X</button>
     </div>
-  );
+  ));
 };
 
 export default History;
--- github-profile-finder/src/components/SearchBar.jsx
+++ github-profile-finder/src/components/SearchBar.jsx
@@ -10,18 +10,30 @@ const SearchBar = ({ getUserInfo }) => {
     setUser(e.target.value);
   };
   const handleSubmit = (e) => {
+    // submit시 새로고침 되는 현상 방지
     e.preventDefault();
+    // user API 받아오기
     getUserInfo(user);
+    // 중복체크
     if (!userList.includes(user)) {
       const newUserList = [...userList, user];
+      // 상태를 변화시켜줌으로써, 화면에 History가 보여지도록 함.
       setUserList(newUserList);
+      // localStorage에 저장하여, 사이트를 껐다가 다시 들어와도 history가 저장되도록 함.
       localStorage.setItem("userList", JSON.stringify(newUserList));
       // localStorage는 모든 데이터를 string으로 변환해버림. 그러므로 JSON 형태로 저장하고(직렬화) 읽어와야(역직렬화) 기본 상태를 유지할 수 있음.
     }
     setUser("");
   };
+  const handleRemove = (user) => {
+    // X 누르면 제거
+    const newUserList = userList.filter((userId) => userId !== user);
+    setUserList(newUserList);
+    localStorage.setItem("userList", JSON.stringify(newUserList));
+  };
 
   useEffect(() => {
+    // 컴포넌트가 mount되었을 때, localStorage 정보를 가지고 옴.
     setUserList(JSON.parse(localStorage.getItem("userList")));
   }, []);
   return (
@@ -34,7 +46,7 @@ const SearchBar = ({ getUserInfo }) => {
           value={user || ""}
         />
       </StyledForm>
-      <History userList={userList} />
+      <History userList={userList} handleRemove={handleRemove} />
     </>
   );
 };
