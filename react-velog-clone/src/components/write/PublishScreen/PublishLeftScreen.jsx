d9bf8d6421d78adbf6ec0c86cac54652005dd1b7
gytks4 <gytks4@naver.com>
2021-10-23 01:09:57 +0900

    feat: publishScreen left 스타일링, textarea 150자로 제한

--- react-velog-clone/src/components/write/PublishScreen/PublishLeftScreen.jsx
+++ react-velog-clone/src/components/write/PublishScreen/PublishLeftScreen.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { colors } from "../../../libs/constants/colors";
 
 const PublishLeftScreen = () => {
+  const [description, setDescription] = useState();
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const length = value.length;
+    if (length > 150) {
+      return;
+    }
+    setDescription(e.target.value);
+  };
   return (
     <StyledRoot>
       <h3>포스트 미리보기</h3>
       <input type="file" />
-      <textarea placeholder="당신의 포스트를 짧게 소개해보세요." />
-      <StyledCharacterCounter>000/150</StyledCharacterCounter>
+      <textarea
+        placeholder="당신의 포스트를 짧게 소개해보세요."
+        value={description}
+        onChange={handleChange}
+      />
+      <StyledCharacterCounter>
+        {(description && description.length) || "0"}/150
+      </StyledCharacterCounter>
     </StyledRoot>
   );
 };
@@ -15,15 +32,27 @@ const PublishLeftScreen = () => {
 export default PublishLeftScreen;
 
 const StyledRoot = styled.div`
+  width: 100%;
   & > h3 {
     margin-bottom: 8px;
+    font-size: 21px;
+    font-weight: bold;
   }
   & > input {
     margin-bottom: 32px;
     display: block;
   }
   & > textarea {
+    width: 100%;
+    height: 118px;
+    resize: none;
+    padding: 12px 16px;
   }
 `;
 
-const StyledCharacterCounter = styled.div``;
+const StyledCharacterCounter = styled.div`
+  text-align: right;
+  font-size: 12px;
+  margin-top: 4px;
+  color: ${colors.dateGray};
+`;
