8e3e07469cd3f8001394107685d186956a4575e9
gytks4 <gytks4@naver.com>
2021-12-01 23:37:41 +0900

    폴더 구조 변경

---
+++ ts-assignment/ts-react-velog-clone/src/pages/Article.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useLocation } from "react-router";
+import Header from "../components/common/Header";
+import Profile from "../components/home/Profile";
+import styled from "styled-components";
+import { colors } from "../libs/constants/colors";
+import ArticleOption from "../components/article/ArticleOption";
+import { StyledTag } from "../components/write/ArticleTag";
+import ImgWrapper from "../components/common/ImgWrapper";
+import { ArticleResponse } from "types";
+
+const Article = () => {
+  const location = useLocation();
+  const article: ArticleResponse = location.state;
+  const { title, body, tags, thumbnail, date } = article;
+  return (
+    <StyledRoot>
+      <Header />
+      <StyledWrapper>
+        <h1>{title}</h1>
+        <ArticleOption article={article} />
+        <StyledInformation>
+          <span>김의진</span>
+          <span>·</span>
+          <span>{date}</span>
+        </StyledInformation>
+        <StyledTags>
+          {tags && tags.map((tag) => <StyledTag key={tag}>{tag}</StyledTag>)}
+        </StyledTags>
+        {thumbnail && (
+          <ImgWrapper ratio="56%">
+            <img src={thumbnail} alt="thumbnail" />
+          </ImgWrapper>
+        )}
+        <StyledContents>{body}</StyledContents>
+        <Profile />
+      </StyledWrapper>
+    </StyledRoot>
+  );
+};
+
+export default Article;
+
+const StyledRoot = styled.div`
+  width: 100%;
+  /* height: 100%; */
+  margin: auto;
+  background-color: ${colors.mainWhite};
+`;
+
+const StyledWrapper = styled.div`
+  max-width: 768px;
+  width: 100%;
+  margin: auto;
+  & > h1 {
+    margin-top: 88px;
+    margin-bottom: 32px;
+    font-size: 36px;
+    font-weight: 800;
+  }
+`;
+
+const StyledInformation = styled.div`
+  margin-top: 24px;
+  & > span:nth-child(1) {
+    color: ${colors.darkBlack};
+    font-weight: bold;
+  }
+  & > span:nth-child(2) {
+    margin: 0 8px;
+  }
+  & > span:nth-child(3) {
+    color: ${colors.lightGray};
+  }
+`;
+
+const StyledTags = styled.div`
+  margin-top: 14px;
+`;
+
+const StyledContents = styled.div`
+  margin-top: 80px;
+`;
